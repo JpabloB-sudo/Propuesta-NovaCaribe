89d996e9916ece770f393aea6dc36bf87141a778
StJp <jpablobarraza@unibarranquilla.edu.co>
2026-07-23 18:12:01 +0000

    Update Firebase SDK imports to latest versions

--- js/contacto-firebase.js
+++ js/contacto-firebase.js
@@ -32,6 +32,7 @@ import {
   serverTimestamp,
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+//Colocar la configuración de firebase real acá
 <script type="module">
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
